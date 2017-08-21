825d837926f0c42a7828fcf0b9adf115a745f92e
JV-BugMaker <794241063@qq.com>
2017-08-21 23:29:36 +0800

    [+]args

---
+++ command_args.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s,seq string
+	for i:=1;i<len(os.Args);i++ {
+		s += seq + os.Args[i]
+		seq = " "
+	}
+
+	var s2,seq2 string
+
+	for _,arg := range os.Args[0:] {
+		//中间用空格 隔开
+		s2 += seq2 + arg
+		seq2 = " "
+	}
+
+	fmt.Println(s,s2)
+}
+
+
